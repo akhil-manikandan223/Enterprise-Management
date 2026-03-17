6d185170b706f22f297903e808a9bbc84d10a5ff
AkhilManikandan <AkhilManikandan@SMM-LP214>
2026-03-17 21:11:33 +0530

    feat: initialize new ASP.NET Core web application with database connection test and OpenAPI support.

--- PerBrain/PerBrain/Program.cs
+++ PerBrain/PerBrain/Program.cs
@@ -1,3 +1,5 @@
+using PerBrain.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<DbConnectionFactory>();
 
 var app = builder.Build();
 
@@ -19,5 +23,22 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var factory = scope.ServiceProvider.GetRequiredService<DbConnectionFactory>();
+    using var connection = factory.CreateConnection();
+
+    try
+    {
+        connection.Open();
+        Console.WriteLine("✅ DATABASE CONNECTED SUCCESSFULLY");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("❌ DB CONNECTION FAILED");
+        Console.WriteLine(ex.Message);
+    }
+}
 
 app.Run();
